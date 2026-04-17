fb9541c140cc96b5adaaa4b2137559d0db5bb8f5
beefcakechimp <136863552+beefcakechimp@users.noreply.github.com>
2026-04-17 17:14:59 +0000

    2

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 
-from backend.app.regimenbank import (
+from regimenbank import (
     Chemotherapy,
     Regimen,
     RegimenBank,
